5d19b08edd43457f61a607b10fe7a4bbcf4dc743
tonyrex99 <59386507+tonyrex99@users.noreply.github.com>
2024-05-19 01:17:16 +0100

    cleaned up code a little

--- app/components/molecules/m-pool/m-pool-create-pool/index.tsx
+++ app/components/molecules/m-pool/m-pool-create-pool/index.tsx
@@ -53,56 +53,46 @@ const CreatePool: React.FC = () => {
   function encodeIntoAtPosition(string: string): Uint8Array {
     return encoder.encode(string);
   }
+  const date = new Date();
+  const isoDateTime = date
+    .toISOString()
+    .replace(/\.\d{3}/, "")
+    .replace(/[-:T]/g, "");
 
   async function createNewPool() {
     if (!algodClient || !serviceClient || !activeAccount) return;
+    const poolName = formData.name;
 
+    const key = poolName + " " + isoDateTime;
     const sp = await algodClient.getTransactionParams().do();
     const appAddress = await serviceClient.appClient.getAppReference();
-    const activeAccountAddress = activeAccount.address;
+    const receiver = appAddress?.appAddress;
+    const sender = activeAccount.address;
     const poolNote = formData.name || "";
+    const amount = 1;
 
-    var txn = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
-      from: activeAccountAddress,
-      to: appAddress?.appAddress,
-      amount: 1,
+    const boxName = encodeIntoAtPosition(isoDateTime);
+    const txn = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
+      from: sender,
+      to: receiver,
+      amount: amount,
       suggestedParams: sp,
       note: encoder.encode(poolNote),
     });
+    const boxes = [{ appId: appAddress?.appId, name: boxName }];
 
-    const date = new Date();
-    const isoDateTime = date
-      .toISOString()
-      .replace(/\.\d{3}/, "")
-      .replace(/[-:T]/g, "");
-    const boxName = encodeIntoAtPosition(isoDateTime);
-    serviceClient.savePool(
-      { key: activeAccountAddress, name: poolNote, txn: txn },
-      { boxes: [{ appId: appAddress?.appId, name: boxName }] }
-    );
+    serviceClient.savePool({ key: key, name: poolNote, txn: txn }, { boxes: boxes });
   }
 
   function createPoolNow() {
     const datatosend = {
       ...formData,
-      key: new Date()
-        .toISOString()
-        .replace(/\.\d{3}/, "")
-        .replace(/[-:T]/g, ""),
+      key: formData.name + " " + isoDateTime,
       manager: activeAccount?.address || "",
       interestRate: Number(formData.interestRate),
       collateralPercentage: Number(formData.collateralPercentage),
     };
-    setFormData((prevData) => ({
-      ...prevData,
-      key: new Date()
-        .toISOString()
-        .replace(/\.\d{3}/, "")
-        .replace(/[-:T]/g, ""),
-      manager: activeAccount?.address || "",
-      interestRate: Number(prevData.interestRate),
-      collateralPercentage: Number(prevData.collateralPercentage),
-    }));
+    setFormData(datatosend);
     console.log("data to send to graphql is: ", datatosend);
     sendCreatePool({ variables: { input: datatosend } })
       .then(() => {
