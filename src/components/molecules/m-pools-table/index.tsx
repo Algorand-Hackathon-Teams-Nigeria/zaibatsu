186b7f014860ad5a18c33794444a13bcddd0d462
tonyrex99 <59386507+tonyrex99@users.noreply.github.com>
2024-07-19 14:45:43 +0100

    added pool card component and page loading bar

--- src/components/molecules/m-pools-table/index.tsx
+++ src/components/molecules/m-pools-table/index.tsx
@@ -22,6 +22,7 @@ import {
 import { Skeleton } from "@/components/ui/skeleton";
 import PoolContributeModal from "@/components/atoms/a-pool-contribute-modal";
 import { PoolAssetHoldingFilter } from "../../../services/graphql/generated";
+import PoolsCard from "@/components/molecules/m-pools-table-mobile/index";
 
 const PoolsTable = () => {
   const listOpts = useAtomValue(listOptionsAtoms.pools);
@@ -32,8 +33,8 @@ const PoolsTable = () => {
   const pools = data?.pools ?? [];
   return (
     <>
-      <Card className="p-2 py-4">
-        <Table className="hidden lg:table" borderless>
+      <Card className="p-2 py-4 hidden lg:block">
+        <Table borderless>
           <TableHeader>
             <TableRow>
               <TableHead>Name</TableHead>
@@ -114,118 +115,12 @@ const PoolsTable = () => {
           </div>
         )}
       </Card>
-      <div className="w-full lg:hidden gap-5 flex flex-col">
-        {pools.length > 0 &&
-          !fetching &&
-          pools.map((pool) => (
-            <Card
-              key={pool.id}
-              className=" gap-3 text-white text-base  leading-[18px] rounded-lg p-3 flex flex-col"
-            >
-              <div className=" border-b-[1px] border-[#F7F7F7] flex flex-col gap-1">
-                <div className="py-2  leading-4 font-bold text-left ">
-                  {pool.name}
-                </div>
-                <div className="py-1  font-normal flex flex-row justify-between ">
-                  <div className="py-2">Assets</div>
-                  <div className="py-2">
-                    {" "}
-                    <AvatarGroup>
-                      {pool.assets.map((asset) => (
-                        <Avatar
-                          className="w-6 h-6 aspect-square"
-                          key={asset.id}
-                        >
-                          <AvatarImage src={asset.imageUrl} />
-                          <AvatarFallback>
-                            {asset.unitName.slice(0, 3).toUpperCase()}
-                          </AvatarFallback>
-                        </Avatar>
-                      ))}
-                    </AvatarGroup>
-                  </div>
-                </div>
-                <div className="py-1  font-normal flex flex-row justify-between ">
-                  <div className="py-2">Net Value</div>
-                  <div className="py-2"> ${pool.netValue.toFixed(2)}</div>
-                </div>
-              </div>
-
-              <div className=" border-b-[1px] border-[#F7F7F7] flex flex-col gap-1">
-                <div className="py-1  font-normal flex flex-row justify-between ">
-                  <div className="py-2">Total Contributors</div>
-                  <div className="py-2"> {pool.totalContributors}</div>
-                </div>
-                <div className="py-1   font-normal flex flex-row justify-between ">
-                  <div className="py-2">Loan Offerings</div>
-                  <div className="py-2">{pool.totalLoanTemplates}</div>
-                </div>
-              </div>
-
-              <div className="pt-1 gap-4 flex flex-row h-10 ">
-                <PoolContributeModal pool={pool} />
-                <Link
-                  className="border border-primary/60 w-full  text-center p-2 rounded-md hover:text-primary-foreground hover:bg-primary/60 transition-all"
-                  href={`/pools/${pool.id}/borrow`}
-                >
-                  Borrow
-                </Link>
-              </div>
-            </Card>
-          ))}
-        {fetching &&
-          Array.from({ length: 10 }).map((_, id) => (
-            <PoolCardSkelton key={id} />
-          ))}
-      </div>
+      <PoolsCard fetching={fetching} pools={pools} />
     </>
   );
 };
 
 export default PoolsTable;
-
-const PoolCardSkelton = () => {
-  return (
-    <Card className=" gap-3 text-white text-base  leading-[18px] rounded-lg p-3 flex flex-col">
-      <div className=" border-b-[1px] border-[#F7F7F7] flex flex-col gap-1">
-        <div className="py-2  leading-4 font-bold text-left ">
-          <Skeleton className="h-8 w-screen max-w-[300px]" />
-        </div>
-        <div className="py-1  font-normal flex flex-row justify-between ">
-          <div className="py-2 mt-2">Assets</div>
-          <div className="py-2">
-            {" "}
-            <AvatarGroup>
-              {Array.from({ length: 3 }).map((_, id) => (
-                <Skeleton key={id} className="w-7 h-7 rounded-full" />
-              ))}
-            </AvatarGroup>
-          </div>
-        </div>
-        <div className="py-1  font-normal flex flex-row justify-between ">
-          <div className="py-2">Net Value</div>
-          <Skeleton className="h-7 max-w-14 w-screen " />
-        </div>
-      </div>
-
-      <div className=" border-b-[1px] border-[#F7F7F7] flex flex-col gap-1">
-        <div className="py-1  font-normal flex flex-row justify-between ">
-          <div className="py-2">Total Contributors</div>
-          <Skeleton className="h-7 max-w-14 w-screen " />
-        </div>
-        <div className="py-1   font-normal flex flex-row justify-between ">
-          <div className="py-2">Loan Offerings</div>
-          <Skeleton className="h-7 max-w-14 w-screen " />
-        </div>
-      </div>
-
-      <div className="pt-1 gap-4 flex flex-row h-10 ">
-        <Skeleton className="w-full h-full text-center  rounded " />
-        <Skeleton className="w-full h-full text-center  rounded " />
-      </div>
-    </Card>
-  );
-};
 
 const PoolRowSkeleton = () => {
   return (
